d8155f59293db84056eea3d3b9c60f88b54ffce9
Kindual <110603179+Kindual@users.noreply.github.com>
2023-04-07 18:26:56 +0300

    complete

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,21 +1,16 @@
-import contactsJson from '../contacts.json'
+// import contactsJson from '../contacts.json'
 const { createSlice } = require("@reduxjs/toolkit");
 
 
 export const contactSlice = createSlice({
     name: 'contact',
     initialState: {
-        contacts: localStorage.getItem('contacts')
-            ? JSON.parse(localStorage.getItem('contacts'))
-            : contactsJson,
-
+        contacts: [],
         filter: '',
     },
     reducers: {
         addContact: (state, action) => {
             state.contacts.push(action.payload);
-            localStorage.setItem('contacts', JSON.stringify(state.contacts))
-
         },
         updateFilter: (state, action) => {
             state.filter = action.payload
@@ -31,3 +26,7 @@ export const { addContact, updateFilter, deleteContact } = contactSlice.actions
 export const selectContacts = (state) => state.contact.contacts;
 export const selectFilter = (state) => state.contact.filter;
 export default contactSlice.reducer;
+
+// localStorage.getItem('contacts')
+//         ? JSON.parse(localStorage.getItem('contacts'))
+//             : contactsJson
